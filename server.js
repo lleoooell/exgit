afeae25caa3d8a383797da245477c45a37e8c86e
Leo Casagrande <leo@greenberry.io>
2020-07-08 16:10:21 +0200

    api setup server people

--- server.js
+++ server.js
@@ -6,9 +6,36 @@ var _ = require('lodash');
 
 var fs = require('fs');
 
-var index = fs.readFileSync('index.html');
+var express = require("express");
+var app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(8080);
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+app.get("/", function(req, res){
+	res.sendFile("./index.html", { root: __dirname });
+});
+app.get("/api/people", (req, res, next) => {
+    res.json([{
+        "name": "John",
+        "age": 30,
+        "cars": ["Ford", "BMW", "Fiat"]
+    }, {
+        "name": "Tom",
+        "age": 24,
+        "cars": ["Ford", "Toyota", "BMW"]
+    }, {
+        "name": "Emma",
+        "age": 34,
+        "cars": ["Ford", "BMW", "Fiat"]
+    }, {
+        "name": "Jack",
+        "age": 29,
+        "cars": ["Jeep", "Mustang", "mehari"]
+    }, {
+        "name": "Sophie",
+        "age": 25,
+        "cars": ["Mini", "Fiat", "Mercedes"]
+    }]);
+});
